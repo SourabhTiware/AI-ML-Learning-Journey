a4215e04d434ee4554b870f88166096a107ea91e
Sourabh Tiware <sourabhtiware1737@gmail.com>
2026-06-19 22:28:24 +0530

    Day 6: AI-ML-DS

---
+++ 01 Python/2 Python Fundamentals Part 2/part_2.py
@@ -0,0 +1,54 @@
+#-----------------------------------------------------------------------------
+# Lec 1: Conditional Statement
+#-----------------------------------------------------------------------------
+print("\nLec 1: Conditional Statement\n")
+
+
+print("Python conditional statement keyword\nif\nelif\nelse")
+
+# print("\nVoting Program")
+
+# age = int(input("\nEnter you age: "))
+
+# if age >= 18:
+#     print("You can vote")
+#     print("you can drive")
+# else:
+#     print("You can't vote")
+#     print("You can't drive")
+
+# print("\nTraffic light Program")
+
+# color =input("Enter traffic light color: ")
+
+# if color == "yellow":
+#     print("look")
+# elif color == "red":
+#     print("wait")
+# elif color == "green":
+#     print("go")
+# else:
+#     print("Wrong color input")
+
+
+#-----------------------------------------------------------------------------
+# Lec 2: Practice Example on Conditional Statement
+#-----------------------------------------------------------------------------
+print("\nLec 2: Practice Example on Conditional Statement\n")
+
+# print("Ques 1: age less than 13 print child \nage is between 13-18 print teenager \nand age is greater than 18 print adult")
+
+# age = int(input("Enter you age: "))
+
+# if(age >= 0 and age < 13):
+#     print("child")
+# elif (age >= 13 and age <18):
+#     print("teenager")
+# elif(age >= 18 and age <=60):
+#     print("adult")
+# elif(age >60):
+#     print("oldage")
+# else:
+#     print("Your input age is wrong")
+
+
